aa7a4dafb9f007a394b0d21988b52e3ceba34e4a
Samuel Cadieux <samuelcadieux@gmail.com>
2015-04-04 18:47:24 -0700

    Adding support to combine and export multiple meshes

--- Editor/OBJEditor.cs
+++ Editor/OBJEditor.cs
@@ -1,50 +1,68 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Collections;
 
-using UnityEngine;
 using UnityEditor;
-
+using UnityEngine;
 using UnityExtension;
+
 
 public class OBJWindow : EditorWindow
 {
 	//------------------------------------------------------------------------------------------------------------
-	private MeshFilter m_MeshFilter = null; 
-	
-    //------------------------------------------------------------------------------------------------------------
-    [MenuItem("OBJ-IO/OBJ Mesh Exporter")]
-    public static void Execute()
-    {
-        OBJWindow.GetWindow<OBJWindow>();
-    }
-	
-    //------------------------------------------------------------------------------------------------------------
-    private void OnGUI()
-    {
-        m_MeshFilter = (MeshFilter)EditorGUILayout.ObjectField("MeshFilter", m_MeshFilter, typeof(MeshFilter), true);
-		
-		if (m_MeshFilter != null)
+	private GameObject m_root = null;
+
+	//------------------------------------------------------------------------------------------------------------
+	[MenuItem("OBJ-IO/OBJ Mesh Exporter")]
+	public static void Execute()
+	{
+		OBJWindow.GetWindow<OBJWindow>();
+	}
+
+	//------------------------------------------------------------------------------------------------------------
+	private void OnGUI()
+	{
+		m_root = (GameObject)EditorGUILayout.ObjectField("Root", m_root, typeof(GameObject), true);
+
+		if (m_root != null)
 		{
+			var meshFilters = new List<MeshFilter>();
+			meshFilters.AddRange(m_root.GetComponents<MeshFilter>());
+			meshFilters.AddRange(m_root.GetComponentsInChildren<MeshFilter>());
+
+			Mesh mesh;
+			if (meshFilters.Count > 0) {
+				CombineInstance[] combine = new CombineInstance[meshFilters.Count];
+				for (int i = 0; i < meshFilters.Count; ++i) {
+					combine[i].mesh = meshFilters[i].sharedMesh;
+					combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+				}
+
+				mesh = new Mesh();
+				mesh.CombineMeshes(combine);
+				mesh.Optimize();
+			} else {
+				mesh = meshFilters[0].sharedMesh;
+			}
+
 			if (GUILayout.Button("Export OBJ"))
 			{
-				var lOutputPath = EditorUtility.SaveFilePanel("Save Mesh as OBJ", "", m_MeshFilter.name + ".obj", "obj");
-				
+				var lOutputPath = EditorUtility.SaveFilePanel("Save Mesh as OBJ", "", m_root.name + ".obj", "obj");
+
 				if (File.Exists(lOutputPath))
 				{
 					File.Delete(lOutputPath);
 				}
-				
+
 				var lStream = new FileStream(lOutputPath, FileMode.Create);
-				var lOBJData = m_MeshFilter.sharedMesh.EncodeOBJ();
+				var lOBJData = mesh.EncodeOBJ();
 				OBJLoader.ExportOBJ(lOBJData, lStream);
 				lStream.Close();
 			}
 		}
 		else
 		{
-			GUILayout.Label("Please provide a MeshFilter");
+			GUILayout.Label("Please provide a GameObject which contains (including it's children)  at least on or more MeshFilter component");
 		}
-    }
+	}
 }
